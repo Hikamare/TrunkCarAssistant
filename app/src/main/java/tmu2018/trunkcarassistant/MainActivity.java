c48aea759950ff5cc91f94cf31048214a6e2c15f
Paweł Motyka <hikamare@gmail.com>
2018-03-01 11:40:51 +0100

    Initial commit

---
+++ app/src/main/java/tmu2018/trunkcarassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package tmu2018.trunkcarassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
