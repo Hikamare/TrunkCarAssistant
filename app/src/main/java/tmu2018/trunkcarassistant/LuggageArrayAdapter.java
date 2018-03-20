30a4175a6bd229b7e868f7f0a49044d8a4267792
Piotr Koter <piotrkoter@gmail.com>
2018-03-20 12:12:22 +0100

    Improved layout in trunk_adapter_layout, add padding in activity_trunk, rename luggage_adapter_layout to trunk_adapter_layout

--- app/src/main/java/tmu2018/trunkcarassistant/LuggageArrayAdapter.java
+++ app/src/main/java/tmu2018/trunkcarassistant/LuggageArrayAdapter.java
@@ -25,7 +25,7 @@ public class LuggageArrayAdapter extends ArrayAdapter<Trunk> {
         Trunk lTrunk = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.luggage_adapter_layout, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.trunk_adapter_layout, parent, false);
         }
         // Lookup view for data population
         TextView carName = (TextView) convertView.findViewById(R.id.carName);
